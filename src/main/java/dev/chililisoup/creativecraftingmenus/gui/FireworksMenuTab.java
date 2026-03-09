ba4e50d5a4e9c04c7c486e99c02a4aec566d41cc
Isaac Roberts <119639439+madebyisaacr@users.noreply.github.com>
2026-03-09 12:59:13 -0400

    Firework slot

--- src/main/java/dev/chililisoup/creativecraftingmenus/gui/FireworksMenuTab.java
+++ src/main/java/dev/chililisoup/creativecraftingmenus/gui/FireworksMenuTab.java
@@ -2,7 +2,10 @@ package dev.chililisoup.creativecraftingmenus.gui;
 
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ResultContainer;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
@@ -18,8 +21,25 @@ public class FireworksMenuTab extends CreativeMenuTab<FireworksMenuTab.Fireworks
     }
 
     public static class FireworksTabMenu extends CreativeMenuTab.CreativeTabMenu<FireworksTabMenu> {
+        private static final ItemStack FIREWORK_ROCKET = Items.FIREWORK_ROCKET.getDefaultInstance();
+
+        private final ResultContainer fireworkSlot = new ResultContainer();
+
         FireworksTabMenu(Player player) {
             super(player);
+            this.addSlot(new Slot(this.fireworkSlot, 0, 166, 33) {
+                @Override
+                public boolean mayPlace(@NotNull ItemStack stack) {
+                    return false;
+                }
+
+                @Override
+                public void onTake(@NotNull Player player, @NotNull ItemStack stack) {
+                    super.onTake(player, stack);
+                    this.container.setItem(0, FIREWORK_ROCKET.copy());
+                }
+            });
+            this.fireworkSlot.setItem(0, FIREWORK_ROCKET.copy());
         }
 
         @Override
